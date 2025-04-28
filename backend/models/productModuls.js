ac02654c9ef45adf17e2adb904c38794f2b215cd
imad mohamed <abmohammedimad00@gmail.com>
2025-04-28 11:53:56 +0400

    finish schema

--- backend/models/productModuls.js
+++ backend/models/productModuls.js
@@ -28,6 +28,60 @@ const productSchema = new mongoose.schema({
     ],
     category:{
         type: string,
-        required: [true, "Please provide a category"]
+        required: [true, "Please provide a category"],
+        enum:{
+            values:[
+                'Electronics',
+                'Mobile Phones',
+                'Laptops',
+                'Accessories',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes/Shoes',
+                'Beauly/Health',
+                'Sports',
+                'Outdoor',
+                'Home'
+            ],
+            message: "Please select a valid category"
+        }
+    },
+    seller:{
+        type: string,
+        required: [true, "Please Enter Product seller"]
+    },
+    stock:{
+        type: Number,
+        required:[true, "Please enter product stock"],
+        maxLength: [20, "Product stock can not exceed 20"],
+        
+    },
+    numberOfReviews:{
+        type: Number,
+        default: 0,
+
+    },
+    reviews:[
+     {
+            name:{
+            type: string,
+            required: true
+
+        },
+        rating:{
+            type: string,
+            required: true
+        },
+        comment:{
+            type: string,
+            required:true
+        }
+     }
+        
+    ],
+    creatAt:{
+        type: Date,
+        default: Date.now()
     }
 })
