397b3bef36a306f1be123e13676682bd86e27131
imad mohamed <abmohammedimad00@gmail.com>
2025-05-12 14:19:52 +0400

    create a router for get single product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,9 +1,12 @@
 const Product = require('../models/productModuls')
 
-exports.getProducts = (req, res, next) =>{
+//Get Products - /api/v1/products
+exports.getProducts = async (req, res, next) =>{
+    const products = await Product.find();
     res.status(200).json({
         success : true,
-        message :  "This route will show all the products in database"
+        count: products.length,
+        products
     })
 }
 
@@ -14,4 +17,22 @@ exports.getProducts = (req, res, next) =>{
         success: true,
         product
     })
+}
+
+//Get Single Product
+exports.getSingleProduct = async (req, res, next) => {
+   const product =  await Product.findById(req.params.id);
+
+   if(!product){
+       return  res.status(404).json({
+            success: false,
+            message: "Product not found"
+         });
+   }
+
+   res.status(201).json({
+        success: true,
+        product
+   })
+
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { get } = require('mongoose');
-const { getProducts, newProduct } = require('../controller/productController');
+const { getProducts, newProduct, getSingleProduct } = require('../controller/productController');
 const router = express.Router();
 
 router.route('/products').get(getProducts);
 router.route('/product/new').post(newProduct);
+router.route('/product/:id').get(getSingleProduct);
 
 module.exports = router;
