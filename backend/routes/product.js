ef2465c5446be3f6b60b2942ab1c5297c9c17f0c
imad mohamed <abmohammedimad00@gmail.com>
2025-05-12 14:42:25 +0400

    add route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { get } = require('mongoose');
-const { getProducts, newProduct, getSingleProduct } = require('../controller/productController');
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controller/productController');
 const router = express.Router();
 
 router.route('/products').get(getProducts);
 router.route('/product/new').post(newProduct);
-router.route('/product/:id').get(getSingleProduct);
+router.route('/product/:id')
+                              .get(getSingleProduct)
+                              .put(updateProduct);
+
 
 module.exports = router;
