8793dd56fca52400c62a34771ed4cbe801fd9bbc
imad mohamed <abmohammedimad00@gmail.com>
2025-05-12 13:06:40 +0400

    seeding datas

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,1 +1,23 @@
-const products = required('../data/product.json');
+const products = require('../data/products');
+const Product = require('../models/productModuls');
+const dotenv = require('dotenv');
+const connectDatabase = require('../config/database')
+
+dotenv.config({path: 'backend/config/config.env'});
+connectDatabase();
+
+const seedProducts = async () => {
+          try{
+              await Product.deleteMany();
+              console.log('Products are deleted!');
+
+              await Product.insertMany(products);
+              console.log('All products are added!');
+          }catch(error){
+                    console.log(error.message);
+          }
+          process.exit();
+          
+}
+
+seedProducts();
