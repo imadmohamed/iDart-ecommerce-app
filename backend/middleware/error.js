75a347aea0921696dfcf8a002f83fddbcc2f75c2
imad mohamed <abmohammedimad00@gmail.com>
2025-05-13 23:14:53 +0400

    envariment based error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,9 +1,20 @@
 module.exports = (err,req,res,next) => {
           err.statusCode = err.statusCode || 500;
 
-          res.status(err.statusCode).json({
-                    success: false,
-                    message: err.message,
-                    stack: err.stack
-          })
+          if(process.env.NODE_ENV == 'Development'){
+                    res.status(err.statusCode).json({
+                              success: false,
+                              message: err.message,
+                              stack: err.stack
+                    })
+          }
+          
+          if(process.env.NODE_ENV == 'Production'){
+                    res.status(err.statusCode).json({
+                              success: false,
+                              message: err.message
+                    })
+          }
+
+          
 }
