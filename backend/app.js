1170503f942fb95a0718f3d6c0fa6d98a30c1c61
imad mohamed <abmohammedimad00@gmail.com>
2025-05-13 23:08:06 +0400

    middleware create

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express =  require('express');
 const app = express();
+const errorMiddleware = require('./middleware/error')
 
 app.use(express.json());
 const products = require('./routes/product')
 
 app.use('/api/v1',products);
 
+app.use(errorMiddleware);
 
 module.exports = app;
