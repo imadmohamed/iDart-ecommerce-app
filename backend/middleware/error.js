45ddd0454c79ea95a3daee40d7e5363fe4ddc92f
imad mohamed <abmohammedimad00@gmail.com>
2025-05-14 14:55:25 +0400

    Handling cast error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,25 +1,49 @@
 const { stack } = require("../app");
+const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err,req,res,next) => {
           err.statusCode = err.statusCode || 500;
 
 
-          if(process.env.NODE_ENV == 'development'){
+          if (process.env.NODE_ENV === 'development') {
+                return res.status(err.statusCode || 500).json({
+                success: false,
+                message: err.message,
+                stack: err.stack,
+                error: err
+          });   
+}
 
-                    return res.status(err.statusCode).json({
-                              success: false,
-                              message: err.message,
-                              stack: err.stack,
-                              });  
-          }
 
-          if(process.env.NODE_ENV == 'production'){
+        //   if(process.env.NODE_ENV == 'production'){
+        //             let message = err.message;
+        //             let error = {...err};
 
-                    return res.status(err.statusCode).json({
-                              success: false,
-                              message: err.message,
-                              });  
-          }
+        //             if(err.name == "ValidationError"){
+        //                       message = Object.values(err.errors).map(value => value.message);
+        //                       error = new ErrorHandler(message, 400);
+        //             }
+        //             return res.status(err.statusCode).json({
+        //                       success: false,
+        //                       message: error.message || "Internal Server Error",
+        //             });  
+        //   }
+
+          if (process.env.NODE_ENV === 'production') {
+                     let message = err.message;
+                     let error = { ...err };
+
+                     if (err.name === "ValidationError") {
+                     message = Object.values(err.errors).map(value => value.message).join(", ");
+                     error = new Error(message);
+                     }         
+
+                     return res.status(error.statusCode || 500).json({
+                     success: false,
+                     message: error.message || "Internal Server Error",
+             });
+}
+
 
                  
 
