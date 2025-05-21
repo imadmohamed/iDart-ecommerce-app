91e311cfca21b15b56f678bd79b52605a41a5d62
imad mohamed <abmohammedimad00@gmail.com>
2025-05-21 17:19:28 +0400

    Hastring the pasword

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
           name:{
@@ -36,6 +37,10 @@ const userSchema = new mongoose.Schema({
           }
 
 
+})
+
+ userSchema.pre("save", async function(next){
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 let model = mongoose.model('User', userSchema)
