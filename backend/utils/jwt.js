38b3503417ed0b22cf9499857cb03173441d3e67
imad mohamed <abmohammedimad00@gmail.com>
2025-05-21 21:24:14 +0400

    sending token cookie

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -3,10 +3,20 @@ const sendToken = (user, statusCode, res) => {
     //Creating JWT Token
     const token = user.getJWTToken();
 
-    res.status(statusCode).json({
+
+    //setting cookies
+
+    const options = {
+        expaires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
+        httpOnly:true,
+    }
+
+    res.status(statusCode)
+    .cookie('token',token, options )
+    .json({
         success: true,
         token,
-        user
+        user,
     })
 
 }
